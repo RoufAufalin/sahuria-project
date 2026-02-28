d96c68ead55afb07568949665935621ed721f1ca
unknown <zhafrankuncoro@gmail.com>
2026-03-01 02:46:00 +0700

    add ui di pages form untuk melakukan pesanan

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import Home from "./pages/HomePage.jsx"
+import OrderForm from "./pages/OrderForm.jsx"
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Home />
+  <StrictMode >
+    <OrderForm/>
   </StrictMode>,
 )
